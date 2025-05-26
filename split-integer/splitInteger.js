145a0d39b3d45b7a4623698ae4e68e81d8dd5ba2
nastushka-oct <kharyakowa2007@gmail.com>
2025-05-26 12:42:02 +0300

    Solution for splitInteger

---
+++ split-integer/splitInteger.js
@@ -0,0 +1,13 @@
+function splitInteger(value, numberOfParts) {
+  const base = Math.floor(value / numberOfParts);
+  const remainder = value % numberOfParts;
+
+  const result = new Array(numberOfParts - remainder).fill(base);
+  result.push(...new Array(remainder).fill(base + 1));
+
+  return result.sort((a, b) => a - b);
+}
+
+module.exports = splitInteger;
+console.log(splitInteger(10, 3)); 
+
